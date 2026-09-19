baaea8219cc0380633a36ef1f4fd73fce1d46b12
Dhruv Sharma <ds3281483@gmail.com>
2026-09-18 23:21:12 -0800

    Add Day 02 loops practice

---
+++ day_2/loops.py
@@ -0,0 +1,74 @@
+# for i in range (1,11):
+#     print(i)
+# for i in range (11,1,-1):
+#     print(i)
+
+# for i in range (1,11):
+#     if i % 2 == 0:
+#          print(i)
+
+# for i in range (1,21):
+#     if i % 3 != 0:
+#         print(i)
+
+# for  i in range (1,11):89
+#     print(5 * i)
+
+# num = int(input("enter number"))
+# for i in range(1,11):
+#     print(num * i)
+
+# for i in range (6,1,-1):
+#     for j in range (i):
+#         print(j,end="")
+#     print()
+
+# fruits = ["Apple","Mango","Banana"]
+
+# for i in fruits:
+#     print(i)
+
+# sum = 0
+# for i in range (1,11):
+#     sum = sum+i
+
+# print(sum)
+
+# for  i  in range (1,11):
+#     if i == 6:
+#         continue
+#     print(i)
+
+# for  i  in range (1,11):
+#     if i == 6:
+#         break
+#     print(i)
+
+# while loop
+# i = 1
+# while i <= 10:
+#     print(i)
+#     i += 1
+
+# i = 10
+# while i >= 1:
+#     print(i)
+#     i -= 1
+
+# i = 2
+# while i <= 20:
+#     print(i)
+#     i += 2
+ 
+# total = 0
+# i = 1
+# while i <= 10:
+#     total += i
+    
+#     i += 1
+# print(total)
+
+i = 1
+while i <= 10:
+    print(5 * i)
+    i+=1
